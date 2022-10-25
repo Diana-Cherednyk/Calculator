8110326d4fd373fdcac4e6e3f972cec633bcf402
Daiana <dana>
2022-10-25 19:47:29 +0300

    Create SecondActivity.kt  and activity.xml

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.calculator
+
+import android.content.Intent
+import android.graphics.Color.green
+import android.graphics.Color.red
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import org.mariuszgromada.math.mxparser.Expression
+import java.text.DecimalFormat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
